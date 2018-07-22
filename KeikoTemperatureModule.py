c8651dc99d70a5f30836f2358a83445d2107d01f
Kim Ortvald <keiko@keikoware.dk>
2018-07-23 01:08:54 +0200

    Update KeikoTemperatureModule.py

--- KeikoTemperatureModule.py
+++ KeikoTemperatureModule.py
@@ -5,18 +5,32 @@ import Adafruit_DHT
 import urllib
 import urllib2
 
-url = 'https://www.keikoware.dk/temp/capture.php'
-sensor = Adafruit_DHT.AM2302
-pin = 4
+def getserial():
+   # Extract serial from cpuinfo file
+   cpuserial = "0000000000000000"
+   try:
+      f = open('/proc/cpuinfo','r')
+      for line in f:
+         if line[0:6]=='Serial':
+            cpuserial = line[10:26]
+      f.close()
+   except:
+      cpuserial = "test323456789"
+   return cpuserial
 
-humidity, temperature = Adafruit_DHT.read_retry(sensor, pin) 
+url = 'https://www.keikoware.dk/temp/capture.php'
+#sensor = Adafruit_DHT.AM2302
+sensor = Adafruit_DHT.DHT22
+pin = 4
+uid = getserial()
+humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
 if humidity is not None and temperature is not None:
     print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
-    payload = {'sensor':'pi_temp_001','temperature':temperature,'humidity':humidity}
+    payload = {'sensor':'pi_temp_001','uid':uid,'temperature':temperature,'humidity':humidity}
     request = urllib2.Request(url,data=urllib.urlencode(payload))
     request.add_header('Content-Type','application/x-www-form-urlencoded')
-    response = urllib2.urlopen(request) 
+    response = urllib2.urlopen(request)
     print response.read()
     print response.code
 else:
